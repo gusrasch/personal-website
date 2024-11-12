c32337b45df464e7cfe9c7167741a48399bb5a87
Gus Rasch <gusrasch@gmail.com>
2024-11-12 11:11:44 -0500

    deprecate gcp infra

--- infra/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "website_ip_address" {
-  value       = google_compute_global_address.website.address
-  description = "The IP address of the website"
-}
--- infra/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-project_id  = "data-rider-430219-h1"
-bucket_name = "gusras-ch-bucket"
-domain_names = ["gusras.ch", "www.gusras.ch"]
